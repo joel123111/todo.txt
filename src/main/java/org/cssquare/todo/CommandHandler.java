2dda9b72720f72a0d9e89d5a4e431998a6dfdf64
Joshua <joshua.smith@live.ca>
2013-03-15 16:56:38 -0300

    Deleted TaskList, finished CommandHandler.java (not Exceptions)

--- src/main/java/org/cssquare/todo/CommandHandler.java
+++ src/main/java/org/cssquare/todo/CommandHandler.java
@@ -3,35 +3,41 @@
 package org.cssquare.todo;
 
 import java.lang.StringBuffer;
+import java.util.ArrayList;
 
 public class CommandHandler{
+	public static void dispatch(String[] args, ArrayList<Task> origTaskList){
 
-	public static dispatch(String args[], TaskList enteredTaskArray){
-
-		String commandResult = "";
+		
 		String command = args[0];
+		String args1 = args[1];
+		String args2 = "";
 
 		//If switch statement to choose which function should be called
 		if(command.equalsIgnoreCase("add")){
-			commandResult = add(args[1], enteredTaskArray);
+			 add(args1, origTaskList);
 
 		}else if(command.equalsIgnoreCase("delete")){
-			commandResult = delete(args[1], enteredTaskArray);
+			 delete(Integer.parseInt(args1), origTaskList);
 
 		}else if(command.equalsIgnoreCase("append")){
-			commandResult = append(enteredTaskArray);
+			 args2 = args[2];
+			 int index = Integer.parseInt(args1);
+			 append(index, args2, origTaskList);
 
 		}else if(command.equalsIgnoreCase("prepend")){
-			return add(enteredTaskArray);
+			 args2 = args[2];
+			 int index = Integer.parseInt(args1);
+			 prepend(index, args2, origTaskList);
 
 		}else if(command.equalsIgnoreCase("replace")){
-			return add(enteredTaskArray);
+			 args2 = args[2];
+			 int index = Integer.parseInt(args1);
+			 replace(index, args2, origTaskList);
 
 		}else{
-			commandResult = "No command was entered";
+			 notValid(origTaskList);
 		}
-
-		return commandResult;
 	}
 
 
@@ -39,52 +45,48 @@ public class CommandHandler{
 	//Commands
 
 	//Adds a Task to the end of the list
-	private String add(Task enteredTaskArray){
-		String result = "";
+	private static CommandResult add(String txt2add, ArrayList<Task> list2addto){
+		Task temp = new Task(txt2add);
+		list2addto.add(temp);
+		String result = "A task has been added";
+		return new CommandResult(list2addto, result);
 
-
-
-		result = "A task has been added";
-
-		return result;
+		
 	}
 	//Deletes the specified task
-	private String delete(Task enteredTaskArray){
-		String result = "";
-
-		
-
-		result = "The task has been deleted";
-
-		return result;
+	private static CommandResult delete(int index, ArrayList<Task> list2deletefrom){
+		list2deletefrom.remove(index);
+		String result = "Task " + index + " has been deleted";
+		return new CommandResult (list2deletefrom, result);
 	}
+
 	//Adds a Task to the end of the list
-	private String append(Task enteredTaskArray){
-		String result = "";
-
-		
-
-		result = "A task has been appended";
-
-		return result;
+	private static CommandResult append(int index, String txt2append, ArrayList<Task> list2appendto){
+		String beginning = list2appendto.get(index).getTask();
+		list2appendto.get(index).setTask(beginning + " " + txt2append);
+		String result = list2appendto.get(index).getTask();
+		return new CommandResult (list2appendto, result);
 	}
+	
 	//Adds a Task to the end of the list
-	private String prepend(Task enteredTaskArray){
-		String result = "";
-
-		
-
-		result = "A task has been prepended";
-
-		return result;
+	private static CommandResult prepend(int index, String txt2prepend, ArrayList<Task> list2prependto){
+		String end = list2prependto.get(index).getTask();
+		list2prependto.get(index).setTask(txt2prepend + " " + end);
+		String result = list2prependto.get(index).getTask();
+		return new CommandResult (list2prependto, result);
 	}
+
 	//Adds a Task to the end of the list
-	private String replace(Task enteredTaskArray){
-		String result = "";
+	private static CommandResult replace(int index, String txt2replace, ArrayList<Task> list2replacewith){
+		String prev_task = list2replacewith.get(index).getTask();
+		Task temp = new Task(txt2replace);
+		list2replacewith.set(index, temp);
+		String result = prev_task + " replaced with: " + list2replacewith.get(index).getTask();
+		return new CommandResult(list2replacewith, result);
+	}
 
-
-		result = "A tastk has been replaced";
-
-		return result;
+	private static CommandResult notValid(ArrayList<Task> unchanged) {
+		String result = "Invalid command";
+		return new CommandResult(unchanged, result);
 	}
 }
